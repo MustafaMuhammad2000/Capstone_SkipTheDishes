710206b9fbdee5c8ffa578ab17ac131e359d2b0f
Zheng Chen <80284414+zheng-628@users.noreply.github.com>
2023-03-13 12:42:21 -0600

    Collapse Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,71 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-//import Button from 'react-bootstrap/Button';
-// import Collapse from 'react-bootstrap/Collapse';
-import { Container,Button,Collapse,Alert,Table } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonCollapse from "./ButtonCollapse";
+import CForm from "./CForm";
+import Results from "./Results";
+import Submit from "./Submit";
+import ClearButton from "./ClearButton";
+import React, { useState } from "react";
+import {
+  Container,
+  Button,
+  Collapse,
+  Alert,
+  Table,
+  Row,
+  Col,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ListContextProvider from "./Context.js";
 
 function App() {
-
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="App">
-      <div className="d-grid gap-2">
-        <Button 
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text"
-          aria-expanded={open}
-          variant="primary" size="lg"
-        >
-          click
-        </Button>
-      </div>
-      
-      <Collapse in={open}>
-        {/* <div id="example-collapse-text">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
-        </div> */}
-        <Table>
-          <tbody>
-          <tr>
-            <td><Button>Test1</Button></td>
-            <td><Button>Test2</Button></td>
-            <td><Button>Test3</Button></td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          </tbody>
-        </Table>
-      </Collapse>
-
-      <Collapse in={open}>
-        {/* <div id="example-collapse-text">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
-        </div> */}
-        <Table>
-          <tbody>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          </tbody>
-        </Table>
-      </Collapse>
+      <ListContextProvider>
+        <CForm />
+        <ClearButton />
+        <Submit />
+      </ListContextProvider>
     </div>
   );
 }
---
+++ my-app/src/Submit.js
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from "react";
+import { ListContext } from "./Context";
+
+const Submit = () => {
+  const { list } = useContext(ListContext);
+  const [response, setResponse] = useState(null);
+
+  const handleSubmit = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/recommend", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(list),
+      });
+
+      const data = await res.json();
+      setResponse(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleSubmit}>Submit List</button>
+      {response && (
+        <div>
+          <h2>Response:</h2>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Submit;
